79da9bb58197c13e4ffb39354824538a0c68041b
tashfeen39 <81779471+tashfeen39@users.noreply.github.com>
2023-12-12 10:04:59 +0500

    made sidepanel urls working

--- portals/urls.py
+++ portals/urls.py
@@ -4,14 +4,14 @@ from . import views
 app_name = 'portals'
 urlpatterns = [
     path("", views.home, name="home"),
-    path("faculty/", views.faculty_dashboard_view, name="dashboard"),
+    path("faculty/dashboard", views.faculty_dashboard_view, name="dashboard"),
     path("faculty/class-info/", views.faculty_class_info_view, name="class-info"),
     path("faculty/complaints/", views.faculty_complaints_view, name="complaints"),
     path("faculty/display-classes/", views.faculty_display_classes_view, name="display-classes"),
     path("faculty/feedbacks/", views.faculty_feedback_view, name="feedbacks"),
     path("faculty/generate-exam/", views.faculty_generate_exam_view, name="generate-exam"),
     path("faculty/grading/", views.faculty_grading_view, name="grading"),
-    path("faculty/enter-marks/", views.faculty_marks_entry_view, name="ter-marks"),
+    path("faculty/enter-marks/", views.faculty_marks_entry_view, name="enter-marks"),
     path("faculty/my-profile/", views.faculty_profile_view, name="my-profile"),
     path("faculty/student-info/", views.faculty_student_info_view, name="student-info"),
 ]
