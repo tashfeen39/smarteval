7fff729817611a12fb3d3dd0e683d6a145a268c9
MUHAMMAD ALI RAZA <107497641+ALI-RAZA-3307@users.noreply.github.com>
2023-12-16 21:12:48 +0500

    Feedback, Complaint, Table in Student Info

---
+++ static/js/Faculty_Complaints.js
@@ -0,0 +1,19 @@
+function submitComplaint() {
+  var name = document.getElementById("name").value;
+  var feedbackSubject = document.getElementById("feedbackSubject").value;
+  var message = document.getElementById("message").value;
+
+  // You can add code here to send the feedback data to a server for processing
+  // For this example, we'll just display the feedback in an alert
+  var feedback =
+    "Name: " +
+    name +
+    "\nFeedback Subject: " +
+    feedbackSubject +
+    "\nMessage:\n" +
+    message;
+  alert("Feedback Submitted:\n" + feedback);
+
+  // Clear the form fields after submission
+  document.getElementById("feedbackForm").reset();
+}
