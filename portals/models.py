5c4a81d14b3c63e10a78cb6834444d42e82aa0a7
MaryamPervez02 <127664353+MaryamPervez02@users.noreply.github.com>
2024-02-07 23:01:55 +0500

    fix migration error

--- portals/models.py
+++ portals/models.py
@@ -25,14 +25,9 @@ class Student(models.Model):
 
 
 class Teacher(models.Model):
-<<<<<<< HEAD
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='teacher')
-    username = models.CharField(max_length=100, null=True, blank=True)
-=======
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, primary_key=True, related_name="teacher"
     )
->>>>>>> be3c1a394142408575f1f28c9f2e46f63fe8a175
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     subject = models.CharField(max_length=100)
@@ -40,10 +35,3 @@ class Teacher(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> be3c1a394142408575f1f28c9f2e46f63fe8a175
